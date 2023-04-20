ecf5da556bfae71b8d9c3927d417024ccd6882a9
MohammedImdadS <imdadsmd@gmail.com>
2023-04-20 19:40:49 +0530

    added scrool-to-top button

--- javaScript.js
+++ javaScript.js
@@ -1,3 +1,9 @@
+  // height of body section
+  $('body').css('padding-top', $('.navbar').outerHeight());
+  var bodyHeight = $('body').height() - $('.navbar').outerHeight();
+  $('fixed-height').css('height', bodyHeight)
+
+  
 //  Nav toggler change
 $(document).ready(function() {
     $('.navbar-toggler').click(function() {
@@ -5,37 +11,67 @@ $(document).ready(function() {
     });
   });
   
-  
-  // height of body section
-  $('body').css('padding-top', $('.navbar').outerHeight());
-  var bodyHeight = $('body').height() - $('.navbar').outerHeight();
-  $('fixed-height').css('height', bodyHeight)
-  
-  
-  
-  // JavaScript code to toggle navbar visibility on scroll
-  $(document).ready(function() {
-    var navbar = $('.navbar'); // Navbar element
-    var navbarHeight = navbar.outerHeight(); // Navbar height
-    var scrollThreshold = 200; // Scroll threshold in pixels
-  
-    // Check scroll position on page load
-    checkNavbarVisibility();
-  
-    // Check scroll position on window scroll
-    $(window).on('scroll', function() {
-      checkNavbarVisibility();
-    });
-  
-    // Function to check scroll position and toggle navbar visibility
-    function checkNavbarVisibility() {
-      var scrollPosition = $(window).scrollTop();
-      if (scrollPosition >= scrollThreshold) {
-        navbar.addClass('hidden');
-      } else {
-        navbar.removeClass('hidden');
-      }
-    }
+
+// Hiding navbar
+const navbar = document.querySelector('.navbar');
+
+// Get the height at which the navbar should be hidden
+const hideHeight = 200;
+
+// Check if the user has scrolled past the hide height
+if (window.pageYOffset > hideHeight) {
+  // Set the navbar to hidden
+  navbar.classList.add('hidden');
+}
+
+// Listen for scroll events
+window.addEventListener('scroll', () => {
+  // Check if the user has scrolled past the hide height
+  if (window.pageYOffset > hideHeight) {
+    // Set the navbar to hidden
+    navbar.classList.add('hidden');
+
+    // Set a flag in localStorage to indicate that the navbar is hidden
+    localStorage.setItem('navbarHidden', 'true');
+  } else {
+    // Set the navbar to visible
+    navbar.classList.remove('hidden');
+
+    // Remove the flag from localStorage
+    localStorage.removeItem('navbarHidden');
+  }
+});
+
+// Check if the navbar should be hidden on page load 
+// (i.e. if the user has already scrolled past the hide height)
+if (localStorage.getItem('navbarHidden') === 'true') {
+  // Set the navbar to hidden
+  navbar.classList.add('hidden');
+}
+
+
+// Scroll-to-top button
+
+var scrollToTopBtn = document.getElementById("scroll-to-top-btn");
+
+// Add a scroll event listener to the window object
+window.addEventListener("scroll", function() {
+  // If the vertical scroll position is greater than or equal to 100px,
+  // show the scroll-to-top button; otherwise, hide it
+  if (window.pageYOffset >= 100) {
+    scrollToTopBtn.classList.remove("d-none");
+  } else {
+    scrollToTopBtn.classList.add("d-none");
+  }
+});
+
+// Add a click event listener to the scroll-to-top button
+scrollToTopBtn.addEventListener("click", function() {
+  // Scroll to the top of the page using smooth scrolling behavior
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
   });
+});
   
   
